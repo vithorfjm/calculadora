64fe79b87f819d20ac232c89465148f94f6b107a
vithorfjm <vithorfjm@outlook.com>
2022-03-06 18:43:28 -0300

    salvando variavel pro primeiro numero e corrigindo primeiro digito

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let botoes = document.querySelectorAll('button');
+const result = document.querySelector("#result");
+var primeiroNum = []
+let botoes = document.querySelectorAll(".button");
+
 for (var i=0; i<botoes.length; i++){
     botoes[i].addEventListener('click', function(){
-        
+            
+            primeiroNum.push(this.innerText);
+            switch(primeiroNum[0]){
+                case '+':
+                    primeiroNum.shift();
+                    break;
+                case 'C':
+                    primeiroNum.shift();
+                    break;
+                case '*':
+                    primeiroNum.shift();
+                    break;
+                case '/':
+                    primeiroNum.shift();
+                    break;
+                case '=':
+                    primeiroNum.shift();
+                    break;
+                default:
+                    result.innerHTML = primeiroNum.join([separador = '']);
+            }
+            console.log(primeiroNum);
+            
+           
     });
 }
