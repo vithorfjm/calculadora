c3dff0b498255bb860ac74a0bc784a7d39dc114d
vithorfjm <vithorfjm@outlook.com>
2022-03-06 19:12:32 -0300

    botão 'corrige' funcionando pro primeiro número digitado

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ let botoes = document.querySelectorAll(".button");
 
 for (var i=0; i<botoes.length; i++){
     botoes[i].addEventListener('click', function(){
-            
-            primeiroNum.push(this.innerText);
-            switch(primeiroNum[0]){
-                case '+':
-                    primeiroNum.shift();
-                    break;
-                case 'C':
-                    primeiroNum.shift();
-                    break;
-                case '*':
-                    primeiroNum.shift();
-                    break;
-                case '/':
-                    primeiroNum.shift();
-                    break;
-                case '=':
-                    primeiroNum.shift();
-                    break;
-                default:
-                    result.innerHTML = primeiroNum.join([separador = '']);
+        primeiroNum.push(this.innerText);
+        switch(primeiroNum[0]){
+            case '+':
+                primeiroNum.shift();
+                break;
+            case 'C':
+                primeiroNum.shift();
+                break;
+            case '*':
+                primeiroNum.shift();
+                break;
+            case '/':
+                primeiroNum.shift();
+                break;
+            case '=':
+                primeiroNum.shift();
+                break;
+            default:
+                result.innerHTML = primeiroNum.join([separador = '']);
+        }
+        for (var i=0; i<primeiroNum.length; i++)
+            if (primeiroNum[i] == 'C'){
+                primeiroNum.splice(0, primeiroNum.length);
+                result.innerHTML = "";
             }
-            console.log(primeiroNum);
+        console.log(primeiroNum);
             
            
     });
